fd8620ca51e34db6bbbc6b86e6981de05a031b67
slashprotocol <84888771+slashprotocol@users.noreply.github.com>
2021-05-28 10:11:11 +0530

    Update MarketNews.js

--- src/components/MarketNews.js
+++ src/components/MarketNews.js
@@ -40,10 +40,9 @@ export default function MarketNews() {
           </li>
           <li>
             <Link to="/news-details">
-              <strong>KCS Pay Fees Feature is Coming Soon</strong>
-              To accelerate the ecosystem construction of KuCoin Share (KCS) and
-              promote the implementation of the KCS application.
-              <span>2019-12-04 20:22</span>
+              <strong>Slash Protocol</strong>
+             Slash Protocol combines best of defi infrastructure including Cross Chain Swap, Decentralized Exchange ,On Chain Option Trading , Largest NFT Market Place.
+              <span>2021-05-04 20:22</span>
             </Link>
           </li>
         </ul>
